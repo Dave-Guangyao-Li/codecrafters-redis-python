b96da7fc3b2820806a9500518e4a7f024ea18fb5
Guangyao (Dave) Li <davidlg158@gmail.com>
2022-12-29 23:19:59 +0000

    stage 1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 # Uncomment this to pass the first stage
 import socket
 # Uncomment this to pass the first stage
-server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-server_socket.accept() # wait for client
+
+def main():
+    print("Logs from your program will appear here!")
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+    server_socket.accept() # wait for client
+
+
+if __name__ == "__main__":
+     main()
